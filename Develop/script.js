c57f911558da70e25c593d98828120525df7ccee
Gerry M <waltermccone88@gmail.com>
2021-01-27 17:12:54 -0600

    Updated the readme and made some minor tweaks to the code to make it easier to understand

--- Develop/script.js
+++ Develop/script.js
@@ -8,34 +8,34 @@ $(document).ready(function () {
     //Time will be variable to measure against
     var currentHour = moment().format('H');
 
-   //create time block elements using Jquery
+    //create time block elements using Jquery
     dateText.text("Today is " + date + ", it is currently " + time)
- 
-    
+
+
     //Create a for loop that writes the timeblocks to the page and the times using moment
-    for (var i = 9; i < 18; i++){
+    for (var i = 9; i < 18; i++) {
 
         //if statement that changes the background color of the calendar slot to future times as green
         if (i > currentHour) {
-           $("#"+i).children("textarea").addClass("future")
+            $("#" + i + "timeBlock").children("textarea").addClass("future")
         }
         //if statement that changes the background color of the calendar slot to past time as gray
         else if (i < currentHour) {
-            $("#"+i).children("textarea").addClass("past")
+            $("#" + i + "timeBlock").children("textarea").addClass("past")
         }
         //if statement that changes the background color of the calendar slot to present time as red
         else {
-            $("#"+i).children("textarea").addClass("present")
+            $("#" + i + "timeBlock").children("textarea").addClass("present")
         }
         // When the for loop iterates also check to see if there is a value saved to the key for that iteration
-        var event = localStorage.getItem(i)
+        var event = localStorage.getItem(i + "timeBlock")
         // Add the value to the text box if it exists in the local storage
-        $("#"+i).children("textarea").val(event)
-   
+        $("#" + i + "timeBlock").children("textarea").val(event)
+
     };
 
     //create a function that saves the text onto the Calendar using the button
-    $(".saveBtn").on("click", function(){
+    $(".saveBtn").on("click", function () {
         // Pull the Id to use as a reference
         var key = $(this).parent().attr("id")
         // Pull the value to save to Local storage
@@ -44,5 +44,5 @@ $(document).ready(function () {
     });
 
 
- 
+
 });
