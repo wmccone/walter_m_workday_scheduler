6ff90f1953af051410ec51255e65239bc3bd8519
Gerry M <waltermccone88@gmail.com>
2021-01-26 20:13:34 -0600

    Cleaned out the extra code and added save buttons. I also Refractored the color coding to use the CSS provided by the excercise

--- Develop/script.js
+++ Develop/script.js
@@ -8,56 +8,26 @@ $(document).ready(function () {
     var textInput = $("textarea")
     var textTime = $(".time-block")
 
-    // moment('1300', 'LTS').format('hh:mm a')
-    // Put the Date into a variable
+
 
    //create time block elements using Jquery
     dateText.text("Today is " + date + ", it is currently " + time)
-    // console.log(currentHour)
-    // var timeBlock = $("<div>");
-    // timeBlock.attr("class", "row time-block")
-    // var hourBlock = $("<p>");
-    // hourBlock.attr("class", "col-md-2 border-start-0 hour")
-    // var textBlock = $("<textarea>")
-    // textBlock.attr("class", "col-md-8")
-    // var saveButton = $("<button>")
-    // saveButton.attr("class", "col-md-2 saveBtn")
+ 
     
-    // //create a function that writes the timeblocks to the page
-    // function createTimeBlock(){
-        
-    //     timeBlock.appendto(".container");
-    //     hourBlock.appendto(".time-block")
-    //     hourBlock.text("hello")
-    //     textBlock.appendto(".time-block")
-    //     saveButton.appendto(".time-block")
-        
-    // }
-    
-    // createTimeBlock()
-    
-
-    // //Create a for loop that writes the timeblocks to the page and the times using moment
-    for (var i = 0; i < 9; i++){
-    //     createTimeBlock()
-    //     // hourBlock.text(moment('9:00').add(i, 'hours').format('LT'))
-
-    };
-    
-    
-    //if statement that changes the background color of the calendar slot to future times as green
+    //Create a for loop that writes the timeblocks to the page and the times using moment
     for (var i = 9; i < 18; i++){
+        //if statement that changes the background color of the calendar slot to future times as green
 
         if (i > currentHour) {
-           $("#"+i).attr("style", "background-color: green")
+           $("#"+i).children("textarea").addClass("future")
         }
         //if statement that changes the background color of the calendar slot to past time as gray
         else if (i < currentHour) {
-            $("#"+i).attr("style", "background-color: gray")
+            $("#"+i).children("textarea").addClass("past")
         }
         //if statement that changes the background color of the calendar slot to present time as yellow
         else {
-            $("#"+i).attr("style", "background-color: yellow")
+            $("#"+i).children("textarea").addClass("present")
         }
 
         var event = localStorage.getItem(i)
@@ -75,14 +45,5 @@ $(document).ready(function () {
     });
     var eventList = JSON.parse(localStorage.getItem(i)) || []
 
-    // function storeEvent() {
-    //     localStorage.setItem("textArea", JSON.stringify(textInput));
-    // }
-    // function renderEvent() {
-    //     for (var i = 9; i < 18; i++)
-    //     // var event = eventList[i]
-        
-
-    // }
-
+ 
 });
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # walter_m_workday_scheduler
 This is homework # 5 of the UTA Full-Stack Coding Bootcamp
+
+The goald of this homework is to create a functioning day planner that displays the current time, list out the hours in a work day, color code the time based on the time of day, and save any entries to local storage.
+
+The local storage should hold the text content on the time slot it was added to.
+
+This is the screenshot of the final product:
+[Day Planner]()
+
+This is the link to the final product in GitHub page.
+
